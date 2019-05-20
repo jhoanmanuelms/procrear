6be21baa2d905c5240acaee2fc062ffdfd3090d5
Jhoan Munoz <jhoanmanuelms@gmail.com>
2019-05-20 13:48:49 +0100

    Initial Commit

---
+++ src/main/java/com/soa/procrear/ProcrearApplication.java
@@ -0,0 +1,13 @@
+package com.soa.procrear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcrearApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcrearApplication.class, args);
+	}
+
+}
