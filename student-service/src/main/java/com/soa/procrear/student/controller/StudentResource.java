86abf5eaf8854b46992deeaed096688ba44d1306
Jhoan Manuel Muñoz Serrano <jhoanmanuelms@gmail.com>
2019-05-21 15:58:57 +0100

    Student Service
    Student available credits

--- student-service/src/main/java/com/soa/procrear/student/controller/StudentResource.java
+++ student-service/src/main/java/com/soa/procrear/student/controller/StudentResource.java
@@ -6,6 +6,7 @@ import com.soa.procrear.student.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,5 +17,12 @@ public class StudentResource {
     @GetMapping("/student/{code}")
     public Student getStudentByCode(@PathVariable Integer code) throws StudentNotFoundException {
         return studentService.getStudentByCode(code).orElseThrow(StudentNotFoundException::new);
+    }
+
+    @GetMapping(value = "/student/{code}/credits")
+    public Integer getStudentCredits(@PathVariable Integer code) throws StudentNotFoundException {
+        Student student = studentService.getStudentByCode(code).orElseThrow(StudentNotFoundException::new);
+
+        return student.getAvailableCredits();
     }
 }
